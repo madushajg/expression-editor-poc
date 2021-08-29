025c45096d2bdbca0ee2e719917018cc3baa5c1f
Madusha <madushajg@gmail.com>
2021-08-29 19:33:35 +0530

    Use function components

--- src/App.js
+++ src/App.js
@@ -1,60 +1,57 @@
 import React, { useState, Component } from "react";
 import './App.css';
 
-class App extends Component {
-  render () {return (
+function App() {
+  return (
       <div className="App">
         <LeftPane />
         <RightPane />
       </div>
-    );
-  }
+  );
 }
 
-class  LeftPane extends Component {
-  render() {
-    return (
-      <div className="App-leftPane">
-        <StatementPane />
-        <ContextSensitivePane />
-      </div>
-    );
-  } 
+function LeftPane() {
+  return (
+    <div className="App-leftPane">
+      <StatementPane />
+      <ContextSensitivePane />
+    </div>
+  );
 }
  
-class  RightPane extends Component {
-  render() {
-    return (
-      <div className="App-rightPane">
-        <div className="App-shortcuts">
-          <h2>Shortcuts</h2>
-        </div>
+function RightPane() {
+  return (
+    <div className="App-rightPane">
+      <div className="App-shortcuts">
+        <h2>Shortcuts</h2>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-class  StatementPane extends Component {
-  render() {
-    return (
-      <div className="App-statement-template">
-        <h1>If statement</h1>
-        <div className="App-statement-template-editor">
-          <h3>if expression</h3>
-        </div>
-      </div>
-    );
-  }
+function StatementPane() {
+  return (
+    <div className="App-statement-template">
+      <h1>If statement</h1>
+      <ExpressionTemplate />
+    </div>
+  );
 }
 
-class  ContextSensitivePane extends Component {
-  render() {
-    return (
-      <div className="App-context-sensitivePane">
-        <h2 style={{color: "green"}}>Suggestions</h2>
-      </div>
-    );
-  }
+function ContextSensitivePane() {
+  return (
+    <div className="App-context-sensitivePane">
+      <h2 style={{color: "green"}}>Suggestions</h2>
+    </div>
+  );
+}
+
+function ExpressionTemplate() {
+  return (
+    <div className="App-statement-template-editor">
+      <h3>if expression</h3>
+    </div>
+  );
 }
 
 export default App;
