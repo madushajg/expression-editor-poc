2bfb162a9b897dad88bec6fa4bcb2dbe6b9b962b
Madusha <madushajg@gmail.com>
2021-08-29 17:10:47 +0530

    Devide main components

--- src/App.js
+++ src/App.js
@@ -1,88 +1,57 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// #################################################################################################
-
-// import React, { useState, Component } from "react";
-// import './App.css';
-
-// function App() {
-//   const [count1, setCount1] = useState(0);
-//   const [count2, setCount2] = useState(0);
-//   return (
-//     <div className="App">
-//       <div className="App-leftPane">
-//         <div className="App-statement-template">
-//           <h1>If statement</h1>
-//           <div className="App-statement-template-editor">
-//             <h3>if expression</h3>
-//           </div>
-//         </div>
-//         <div className="App-context-sensitivePane">
-//           <h2 style={{color: "green"}}>Suggestions</h2>
-//         </div>
-//       </div>
-//       <div className="App-rightPane">
-//         <div className="App-shortcuts">
-//           <h2>Shortcuts</h2>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// #################################################################################################
-
-
-
-
 import React, { useState, Component } from "react";
 import './App.css';
 
 class App extends Component {
   render () {return (
       <div className="App">
-        <div className="App-leftPane">
-          <div className="App-statement-template">
-            <h1>If statement</h1>
-            <div className="App-statement-template-editor">
-              <h3>if expression</h3>
-            </div>
-          </div>
-          <div className="App-context-sensitivePane">
-            <h2 style={{color: "green"}}>Suggestions</h2>
-          </div>
+        <LeftPane />
+        <RightPane />
+      </div>
+    );
+  }
+}
+
+class  LeftPane extends Component {
+  render() {
+    return (
+      <div className="App-leftPane">
+        <StatementPane />
+        <ContextSensitivePane />
+      </div>
+    );
+  } 
+}
+ 
+class  RightPane extends Component {
+  render() {
+    return (
+      <div className="App-rightPane">
+        <div className="App-shortcuts">
+          <h2>Shortcuts</h2>
         </div>
-        <div className="App-rightPane">
-          <div className="App-shortcuts">
-            <h2>Shortcuts</h2>
-          </div>
+      </div>
+    );
+  }
+}
+
+class  StatementPane extends Component {
+  render() {
+    return (
+      <div className="App-statement-template">
+        <h1>If statement</h1>
+        <div className="App-statement-template-editor">
+          <h3>if expression</h3>
         </div>
+      </div>
+    );
+  }
+}
+
+class  ContextSensitivePane extends Component {
+  render() {
+    return (
+      <div className="App-context-sensitivePane">
+        <h2 style={{color: "green"}}>Suggestions</h2>
       </div>
     );
   }
